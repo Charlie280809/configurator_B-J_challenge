e6e9e214a42cb419d24966f10fae78d6e618779e
Charlie Van Belle <r0995521@student.thomasmore.be>
2026-07-03 21:09:30 +0200

    fix: adjust lighting

--- src/main.js
+++ src/main.js
@@ -69,10 +69,10 @@ window.addEventListener('resize', () => {
 });
 
 // Light setup
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.5);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.2);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
 directionalLight.position.set(5, 10, 7.5);
 directionalLight.castShadow = true;
 scene.add(directionalLight);
